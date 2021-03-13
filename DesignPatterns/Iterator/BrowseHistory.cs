25e6e10c589e6ca4f4636ab29133229953edc51e
NaelKilani <nael.kilani@stu.najah.edu>
2021-03-13 19:17:49 +0200

    Iterator Pattern.

---
+++ DesignPatterns/Iterator/BrowseHistory.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Iterator
+{
+    public class BrowseHistory<T>
+    {
+        private readonly List<T> _history = new List<T>();
+
+        public void Push(T item)
+        {
+            _history.Add(item);
+        }
+
+        public T Pop()
+        {
+            var lastIndex = _history.Count - 1;
+
+            var item = _history.ElementAt(lastIndex);
+            _history.RemoveAt(lastIndex);
+
+            return item;
+        }
+
+        public IIterator<T> GetIterator()
+        {
+            return new ListIterator(this);
+        }
+
+        private class ListIterator : IIterator<T>
+        {
+            private readonly BrowseHistory<T> _browseHistory;
+            private int _index;
+
+            public ListIterator(BrowseHistory<T> browseHistory)
+            {
+                _browseHistory = browseHistory;
+            }
+
+            public bool HasNext()
+            {
+                return _index < _browseHistory._history.Count;
+            }
+
+            public T Current()
+            {
+                return _browseHistory._history.ElementAt(_index);
+            }
+
+            public void Next()
+            {
+                _index++;
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Iterator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Iterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var browserHistory = new BrowseHistory<string>();
+
+            browserHistory.Push("www.google.com");
+            browserHistory.Push("www.amazon.com");
+            browserHistory.Push("www.xngage.com");
+
+            var iterator = browserHistory.GetIterator();
+            while (iterator.HasNext())
+            {
+                Console.WriteLine(iterator.Current());
+
+                iterator.Next();
+            }
+        }
+    }
+}
